87ad6ee45ac8305fe4f7a03a4211f3b0edc60688
Laís Escorcio <61030205+laisescorcio@users.noreply.github.com>
2025-02-16 17:23:14 -0300

    feat: get data on JSON and HTTP requests

--- src/server.js
+++ src/server.js
@@ -2,8 +2,24 @@ import http from "node:http";
 
 const users = [];
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req;
+
+  const buffers = []; // when you need to wait all chunks to be returned to read to transform or write
+
+  for await (const chunk of req) {
+    // to wait each slice of the streams to be returned
+    buffers.push(chunk); // insert in the buffer array
+  }
+
+  try {
+    // create new prop inside req called body
+    req.body = JSON.parse(Buffer.concat(buffers).toString());
+    // after all chunks are readed
+    // JSON.parse() to transform String to JSON
+  } catch {
+    req.body = null;
+  }
 
   if (method === "GET" && url === "/users") {
     return res
@@ -12,10 +28,12 @@ const server = http.createServer((req, res) => {
   }
 
   if (method === "POST" && url === "/users") {
+    const { name, email } = req.body;
+
     users.push({
       id: 1,
-      name: "Laís Escorcio",
-      email: "escorcio.lais@gmail.com",
+      name,
+      email,
     });
 
     return res.writeHead(201).end(); // Status code 201 => Created
